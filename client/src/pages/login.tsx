53252cadbe2822c0c0b9a5d7ea0e72c30b0a1228
anton lokianov <antonlokianov@gmail.com>
2024-03-27 18:21:47 +0200

    Refactor login page layout

--- client/src/pages/login.tsx
+++ client/src/pages/login.tsx
@@ -6,9 +6,9 @@ import { Card } from "@/components/ui/card";
 
 export const Login = () => {
   return (
-    <section className="container py-16 h-full gap-4">
-      <Card className="flex flex-1 h-full items-center p-3 divide-x-2">
-        <div className="w-2/4 flex flex-col gap-5 flex-1 p-6 items-center">
+    <section className="container py-14 h-full">
+      <Card className="flex flex-1 h-full items-center p-2 divide-x-2">
+        <div className="w-2/4 flex flex-col gap-5 flex-1 p-4 items-center">
           <p className="text-center text-xl">Login to enjoy our CRM</p>
           <LogIn className="mx-auto text-primary animate-pulse" size={48} />
           <SignInForm />
