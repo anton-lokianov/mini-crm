8357bac77beb0d7bceaf100ff3197b3e9553c96c
anton lokianov <antonlokianov@gmail.com>
2024-04-10 16:57:26 +0300

    Refactor role-based routes and remove adminRoutes.tsx

--- client/src/routes/mainRoutes.tsx
+++ client/src/routes/mainRoutes.tsx
@@ -7,7 +7,7 @@ import Page404 from "@/pages/page404";
 import UserSettings from "@/pages/userSettings";
 import RootRoutes from "@/routes/rootRoutes";
 import AuthRoutes from "./authRoutes";
-import AdminRoutes from "./adminRoutes";
+import RoleBaseRoutes from "./roleBaseRoutes";
 import DriversPanel from "@/pages/driversPanel";
 import ClientsPanel from "@/pages/clientsPanel";
 import ShiftsPanel from "@/pages/shiftsPanel";
@@ -22,12 +22,19 @@ const MainRoutes = () => {
         </Route>
         <Route element={<AuthRoutes />}>
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route element={<AdminRoutes />}>
+          <Route element={<RoleBaseRoutes roles={["admin"]} />}>
             <Route path="/user/settings" element={<UserSettings />} />
           </Route>
-          <Route path="/drivers-panel" element={<DriversPanel />} />
-          <Route path="/clients-panel" element={<ClientsPanel />} />
-          <Route path="/shifts-panel" element={<ShiftsPanel />} />
+          <Route
+            element={
+              <RoleBaseRoutes roles={["admin", "manager", "operator"]} />
+            }>
+            <Route path="/drivers-panel" element={<DriversPanel />} />
+          </Route>
+          <Route element={<RoleBaseRoutes roles={["admin", "manager"]} />}>
+            <Route path="/clients-panel" element={<ClientsPanel />} />
+            <Route path="/shifts-panel" element={<ShiftsPanel />} />
+          </Route>
         </Route>
         <Route path="*" element={<Page404 />} />
       </Routes>
---
+++ client/src/routes/roleBaseRoutes.tsx
@@ -0,0 +1,20 @@
+import { useAuthStore } from "@/service/store/auth-store";
+import { Navigate, Outlet } from "react-router-dom";
+
+type Role = "admin" | "manager" | "operator" | "service";
+
+type Props = {
+  roles: Role[];
+};
+
+const RoleBaseRoutes = ({ roles }: Props) => {
+  const user = useAuthStore((state) => state.user);
+
+  if (!roles.includes(user!.role as Role)) {
+    return <Navigate to="/dashboard" replace={true} />;
+  }
+
+  return <Outlet />;
+};
+
+export default RoleBaseRoutes;
