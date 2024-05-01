b1664b07c0c86456f04d9d88bad701911cc47880
anton lokianov <antonlokianov@gmail.com>
2024-05-01 08:28:40 +0300

    Refactor page404 component to use Card and Button components for better UI

--- client/src/pages/page404.tsx
+++ client/src/pages/page404.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { useNavigate } from "react-router-dom";
 
 const Page404 = () => {
-  return <div>Page404</div>;
+  const navigate = useNavigate();
+  return (
+    <div className="h-full flex justify-center items-center">
+      <Card>
+        <CardHeader>
+          <h3 className="text-2xl font-semibold leading-none tracking-tight">
+            Page Not Found (404)
+          </h3>
+        </CardHeader>
+        <CardContent>
+          <p className="text-sm text-muted-foreground">
+            The page you are looking for does not exist.
+          </p>
+          <Button className="mt-6 text-foreground" onClick={() => navigate(-1)}>
+            go back
+          </Button>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default Page404;
