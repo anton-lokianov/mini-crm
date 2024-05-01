2ba7cddaf40ebb684cb81146e1041bbb38feefc7
anton lokianov <antonlokianov@gmail.com>
2024-05-01 08:19:41 +0300

    Refactor validation middleware to support dynamic DTO functions

--- server/src/middlewares/validation.ts
+++ server/src/middlewares/validation.ts
@@ -1,10 +1,22 @@
 import { Request, Response, NextFunction } from "express";
-import { validationResult } from "express-validator";
+import { ValidationChain, validationResult } from "express-validator";
 
-export const validation = (req: Request, res: Response, next: NextFunction) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(422).json({ errors: errors.array() });
-  }
-  next();
+type DTOFunction = () => ValidationChain[];
+
+export const validation = (dtoFunction: DTOFunction) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    const dto = dtoFunction();
+    for (let validationRule of dto) {
+      await validationRule.run(req);
+    }
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({
+        message: "Validation failed, check your data.",
+        errors: errors.array(),
+      });
+    }
+    next();
+  };
 };
