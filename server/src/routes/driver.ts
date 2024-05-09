600620a82c0014c2b914221d3e1b1e6dedafb36b
anton lokianov <antonlokianov@gmail.com>
2024-05-09 20:52:46 +0300

    chore: Update driver route to restrict createDriver and getAllDrivers endpoints to admin, manager, and operator roles

--- server/src/routes/driver.ts
+++ server/src/routes/driver.ts
@@ -2,13 +2,29 @@ import express from "express";
 
 import { verifyToken } from "../middlewares/verifyToken";
 import driverControllers from "../controllers/driver";
+import { roleAuth } from "../middlewares/roleAuth";
 
 const router = express.Router();
 
-router.post("/createDriver", verifyToken, driverControllers.createDriver);
+router.post(
+  "/createDriver",
+  verifyToken,
+  roleAuth({ roles: ["admin", "manager"] }),
+  driverControllers.createDriver
+);
 
-router.get("/getAllDrivers", verifyToken, driverControllers.getAllDrivers);
+router.get(
+  "/getAllDrivers",
+  verifyToken,
+  roleAuth({ roles: ["admin", "manager", "operator"] }),
+  driverControllers.getAllDrivers
+);
 
-router.post("/openShift", verifyToken, driverControllers.handleDriverShift);
+router.post(
+  "/openShift",
+  verifyToken,
+  roleAuth({ roles: ["admin", "manager", "operator"] }),
+  driverControllers.handleDriverShift
+);
 
 export default router;
